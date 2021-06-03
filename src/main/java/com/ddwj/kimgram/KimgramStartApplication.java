ede4a85b20c187f32f0980249692d3bea7ac8ef0
JHKIMS <wjdgns5131@gmail.com>
2021-06-03 11:13:24 +0900

    kimgram 기본 세팅 완료

---
+++ src/main/java/com/ddwj/kimgram/KimgramStartApplication.java
@@ -0,0 +1,13 @@
+package com.ddwj.kimgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimgramStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimgramStartApplication.class, args);
+	}
+
+}
